4a465f9b4a2341b1b6423a4dbe8247e65ce32326
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-30 19:53:46 -0800

    urldecode(newId) so that illegal characters like spaces are now ok in the url and ok in the id

--- EZCheck2_Module/EZCheck2_Module.ino
+++ EZCheck2_Module/EZCheck2_Module.ino
@@ -215,7 +215,7 @@ void createWebServerApi(){
     webServer.sendHeader("Connection", "close");
     webServer.send(200, "text/plain", "OK");
     String newId = webServer.pathArg(0);
-    preferences.putString("id", newId);
+    preferences.putString("id", urldecode(newId));
     ESP.restart();
   });
   webServer.on(UriBraces("/api/setTar/{}"), HTTP_POST, []() {
@@ -304,4 +304,39 @@ void tprint(IPAddress x){
 void tclear(){
   if (!sim) lcd.clear();
   else Serial.println();
+}
+unsigned char h2int(char c) {
+    if (c >= '0' && c <='9'){
+        return((unsigned char)c - '0');
+    }
+    if (c >= 'a' && c <='f'){
+        return((unsigned char)c - 'a' + 10);
+    }
+    if (c >= 'A' && c <='F'){
+        return((unsigned char)c - 'A' + 10);
+    }
+    return(0);
+}
+String urldecode(String str) {
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    for (int i =0; i < str.length(); i++){
+        c=str.charAt(i);
+      if (c == '+'){
+        encodedString+=' ';  
+      }else if (c == '%') {
+        i++;
+        code0=str.charAt(i);
+        i++;
+        code1=str.charAt(i);
+        c = (h2int(code0) << 4) | h2int(code1);
+        encodedString+=c;
+      } else{
+        encodedString+=c;  
+      }
+      yield();
+    }
+   return encodedString;
 }
