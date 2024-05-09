be204e16baad436f33843eb20caceaa2b00294e1
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-09 13:38:56 -0700

    format EZCheck2_Module

--- EZCheck2_Module/EZCheck2_Module.ino
+++ EZCheck2_Module/EZCheck2_Module.ino
@@ -17,7 +17,7 @@
 
 //Webserver
 WebServer webServer(80);
-String signInPath; 
+String signInPath;
 String signOutPath;
 
 //Global
@@ -25,9 +25,10 @@ bool sim = false;
 bool signedIn = false;
 String user = "";
 String pass = "";
+String apiKey = "";
 
 //Preferences
-Preferences preferences; 
+Preferences preferences;
 String id;
 String safeId;
 String tar;
@@ -39,10 +40,10 @@ bool isSTA;
 const int ROW_NUM = 4;
 const int COLUMN_NUM = 4;
 char keys[ROW_NUM][COLUMN_NUM] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
 };
 byte pin_rows[ROW_NUM] = {16, 4, 2, 15};
 byte pin_column[COLUMN_NUM] = {19, 18, 5, 17};
@@ -53,49 +54,55 @@ const int rs = 32, en = 33, d4 = 25, d5 = 26, d6 = 27, d7 = 14;
 LiquidCrystal lcd = LiquidCrystal(rs, en, d4, d5, d6, d7);
 
 //Gasher
-const int red = 22; 
+const int red = 22;
 const int green = 23;
 
-void setup(){
+void setup() {
   //activate
   Serial.begin(115200);
   if (!sim) lcd.begin(16, 2);
   preferences.begin("app", false);
-  pinMode(red,OUTPUT); digitalWrite(red,LOW);
-  pinMode(green,OUTPUT); digitalWrite(green,LOW);
+  pinMode(red, OUTPUT); digitalWrite(red, LOW);
+  pinMode(green, OUTPUT); digitalWrite(green, LOW);
 
   //load preferences
   id = preferences.getString("id", DEFAULT_ID); if (!id || id.length() <= 4) id = DEFAULT_ID;
-  safeId = id; safeId.replace(" ","-");
+  safeId = id; safeId.replace(" ", "-");
   tar = preferences.getString("tar", DEFAULT_TAR);
-  signInPath = "https://"+tar + "/api/post/join-machine";
-  signOutPath = "https://"+tar + "/api/post/leave-machine";
+  signInPath = "https://" + tar + "/api/post/join-machine";
+  signOutPath = "https://" + tar + "/api/post/leave-machine";
   network = preferences.getString("network", DEFAULT_NETWORK);
   password = preferences.getString("password", DEFAULT_PASSWORD);
   isSTA = preferences.getBool("isSTA", true);
-  
+
   //begin wifi
   WiFi.setHostname(safeId.c_str());
-  if (isSTA){
-    WiFi.begin(network.c_str(), password.c_str()); 
-    tclear();tprint("Connecting");
+  if (isSTA) {
+    WiFi.begin(network.c_str(), password.c_str());
+    tclear(); tprint("Connecting");
     int cnt = 0;
-    while(WiFi.status() != WL_CONNECTED && cnt <= 22) {
+    while (WiFi.status() != WL_CONNECTED && cnt <= 22) {
       cnt++;
-      if (cnt==7) lcd.setCursor(0, 1);
+      if (cnt == 7) lcd.setCursor(0, 1);
       tprint(".");
       delay(1000);
-    }  
-    if (WiFi.status() == WL_CONNECTED) {tclear();tprint("Online");}
-    else {tclear();tprint("Offline");} 
+    }
+    if (WiFi.status() == WL_CONNECTED) {
+      tclear();
+      tprint("Online");
+    }
+    else {
+      tclear();
+      tprint("Offline");
+    }
   } else {
     WiFi.softAP(id.c_str(), ADMIN_PASSWORD);
-    tclear();tprint("AP");
+    tclear(); tprint("AP");
   }
-  tprint(" V");tprint(STAMP);
-  lcd.setCursor(0, 1);tprint(id);
+  tprint(" V"); tprint(STAMP);
+  lcd.setCursor(0, 1); tprint(id);
   MDNS.begin(safeId.c_str());
-  
+
   //begin webserver
   createWebServerApi();
   webServer.begin();
@@ -105,127 +112,126 @@ void setup(){
 void loop() {
   webServer.handleClient(); //handle webserver clients
   char key = tread(); //handle keypress
-  if (key){
+  if (key) {
     if (signedIn) signOut();
-    if (key=='D') { //D -> pop pass
-      if (pass.length()>0) pass.remove(pass.length()-1);
-      tclear();tprintlong("PIN: "+hide(pass));
-    } else if (key=='*') { //* -> submit pass
-      if (pass == ADMIN_PASSWORD){ //toggle STA/AP 
-        preferences.putBool("isSTA",!isSTA);
+    if (key == 'D') { //D -> pop pass
+      if (pass.length() > 0) pass.remove(pass.length() - 1);
+      tclear(); tprintlong("PIN: " + hide(pass));
+    } else if (key == '*') { //* -> submit pass
+      if (pass == ADMIN_PASSWORD) { //toggle STA/AP
+        preferences.putBool("isSTA", !isSTA);
         ESP.restart();
       } else signIn();
     } else { //all other keys -> add char to pass
       pass.concat(key);
-      tclear();tprintlong("PIN: "+hide(pass));
+      tclear(); tprintlong("PIN: " + hide(pass));
     }
   }
   delay(1);
 }
 
-boolean signIn(){
-  //handle connected 
-  if(WiFi.status() == WL_CONNECTED){
+boolean signIn() {
+  //handle connected
+  if (WiFi.status() == WL_CONNECTED) {
     //send request
-    WiFiClientSecure client;client.setInsecure();HTTPClient http;
+    WiFiClientSecure client; client.setInsecure(); HTTPClient http;
     http.begin(client, signInPath);
     http.addHeader("Content-Type", "application/json");
     DynamicJsonDocument doc(1024);
     doc["machineName"] = id;
     doc["studentPIN"] = pass;
     doc["IP"] = WiFi.localIP();
-    String msg;serializeJson(doc,msg);
+    String msg; serializeJson(doc, msg);
     int resCode = http.POST(msg);
     //handle response
-    String res = http.getString();http.end();
-    if (resCode==200) {
-      user = res;pass = "";signedIn = true;
-      tclear();tprintlong(res);digitalWrite(green,HIGH);digitalWrite(red,LOW);
+    String res = http.getString(); http.end();
+    if (resCode == 200) {
+      user = res; pass = ""; signedIn = true;
+      tclear(); tprintlong(res); digitalWrite(green, HIGH); digitalWrite(red, LOW);
       return true;
     } else {
-      user = "";pass = "";signedIn = false;
-      tclear();tprintlong(res);digitalWrite(red,HIGH);
+      user = ""; pass = ""; signedIn = false;
+      tclear(); tprintlong(res); digitalWrite(red, HIGH);
       return false;
     }
   }
   //handle disconnected
-  tclear();tprint("Offline");
+  tclear(); tprint("Offline");
   return false;
 }
 
-boolean signOut(){
-  //handle connected 
-  if(WiFi.status() == WL_CONNECTED){
+boolean signOut() {
+  //handle connected
+  if (WiFi.status() == WL_CONNECTED) {
     //send request
-    WiFiClientSecure client;client.setInsecure();HTTPClient http;
+    WiFiClientSecure client; client.setInsecure(); HTTPClient http;
     http.begin(client, signOutPath);
     http.addHeader("Content-Type", "application/json");
     DynamicJsonDocument doc(1024);
     doc["machineName"] = id;
-    String msg;serializeJson(doc, msg);
+    String msg; serializeJson(doc, msg);
     int resCode = http.POST(msg);
     //handle res
-    String res = http.getString();http.end();
+    String res = http.getString(); http.end();
     if (resCode == 200) {
-      user = "";pass = "";signedIn = false;
-      tclear();tprintlong(res);digitalWrite(green, LOW);
+      user = ""; pass = ""; signedIn = false;
+      tclear(); tprintlong(res); digitalWrite(green, LOW);
       return true;
     }
     else {
-      user = "";pass = "";signedIn = true;
-      tclear();tprintlong(res);digitalWrite(red, HIGH);
+      user = ""; pass = ""; signedIn = true;
+      tclear(); tprintlong(res); digitalWrite(red, HIGH);
       return false;
     }
   }
   //handle disconnected
-  tclear();tprint("Offline");
+  tclear(); tprint("Offline");
   return false;
 }
 
-void createWebServerApi(){
+void createWebServerApi() {
   //PAGES
-  //home page with main forms
   webServer.on("/", HTTP_GET, []() {
     webServer.sendHeader("Connection", "close");
     webServer.send(200, "text/html", indexHtml);
   });
-  //ota
-  webServer.on("/ota", HTTP_GET, []() {
-    webServer.sendHeader("Connection", "close");
-    webServer.send(200, "text/html", otaHtml);
-  });
-  //about
-  webServer.on("/about", HTTP_GET, []() {
-    webServer.sendHeader("Connection", "close");
-    webServer.send(200, "text/html", aboutHtml);
-  });
-  
+
   //API
-  //ping
-  webServer.on("/api/ping", HTTP_POST, []() {
-    webServer.sendHeader("Connection", "close");
-    webServer.send(200, "text/plain", "pong");
-  });
   //preferences CRUD
-  webServer.on("/api/factoryReset", HTTP_GET, []() {
+  webServer.on("/api/reset/{}", HTTP_POST, []() {
     webServer.sendHeader("Connection", "close");
-    webServer.send(200, "text/html", "factory reset.");
-    preferences.clear();
-    ESP.restart();
+    String key = webServer.pathArg(0);
+    if (key == ADMIN_PASSWORD) {
+      webServer.send(200, "text/plain", "OK");
+      preferences.clear();
+      ESP.restart();
+    } else {
+      webServer.send(500, "text/plain", "BAD KEY");
+    }
   });
-  webServer.on(UriBraces("/api/setId/{}"), HTTP_POST, []() {
+  webServer.on(UriBraces("/api/setId/{}/{}"), HTTP_POST, []() {
     webServer.sendHeader("Connection", "close");
-    webServer.send(200, "text/plain", "OK");
     String newId = webServer.pathArg(0);
-    preferences.putString("id", urldecode(newId));
-    ESP.restart();
+    String key = webServer.pathArg(1);
+    if (key == ADMIN_PASSWORD) {
+      webServer.send(200, "text/plain", "OK");
+      preferences.putString("id", urldecode(newId));
+      ESP.restart();
+    } else {
+      webServer.send(500, "text/plain", "BAD KEY");
+    }
   });
-  webServer.on(UriBraces("/api/setTar/{}"), HTTP_POST, []() {
+  webServer.on(UriBraces("/api/setTar/{}/{}"), HTTP_POST, []() {
     webServer.sendHeader("Connection", "close");
-    webServer.send(200, "text/plain", "OK");
     String newTar = webServer.pathArg(0);
-    preferences.putString("tar", newTar);
-    ESP.restart();
+    String key = webServer.pathArg(1);
+    if (key == ADMIN_PASSWORD) {
+      webServer.send(200, "text/plain", "OK");
+      preferences.putString("tar", newTar);
+      ESP.restart();
+    } else {
+      webServer.send(500, "text/plain", "BAD KEY");
+    }
   });
   webServer.on(UriBraces("/api/getInfo"), HTTP_GET, []() {
     webServer.sendHeader("Connection", "close");
@@ -238,17 +244,23 @@ void createWebServerApi(){
     doc["defaultId"] = DEFAULT_ID;
     doc["signInPath"] = signInPath;
     doc["signOutPath"] = signOutPath;
-    doc["IP"] = isSTA?WiFi.localIP():WiFi.softAPIP();
+    doc["IP"] = isSTA ? WiFi.localIP() : WiFi.softAPIP();
     String msg; serializeJson(doc, msg);
     webServer.send(200, "text/plain", msg);
   });
   //handling uploading firmware file
-  webServer.on("/update", HTTP_POST, []() {
+  webServer.on(UriBraces("/update/{}"), HTTP_POST, []() {
     webServer.sendHeader("Connection", "close");
-    webServer.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
-    ESP.restart();
+    apiKey = webServer.pathArg(0);
+    if (apiKey == ADMIN_PASSWORD) {
+      webServer.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+      ESP.restart();
+    } else {
+      webServer.send(500, "text/plain", "BAD KEY");
+    }
   }, []() {
     HTTPUpload& upload = webServer.upload();
+    if (apiKey == ADMIN_PASSWORD) {
     if (upload.status == UPLOAD_FILE_START) {
       Serial.printf("Update: %s\n", upload.filename.c_str());
       if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
@@ -267,84 +279,85 @@ void createWebServerApi(){
         Update.printError(Serial);
       }
     }
+    }
   });
-} 
+}
 
 //io
-char tread(){
+char tread() {
   if (!sim) return pad.getKey();
   else if (Serial.available()) return Serial.read();
   else return '\0';
 }
-void tprintlong(String x){
+void tprintlong(String x) {
   if (!sim) {
     if (x.length() > 16) {
-      lcd.print(x.substring(0,16));
+      lcd.print(x.substring(0, 16));
       lcd.setCursor(0, 1);
       lcd.print(x.substring(16));
     } else {
       lcd.print(x);
     }
   }
-  else Serial.print(x);
+  Serial.print(x);
 }
-void tprint(String x){
+void tprint(String x) {
   if (!sim) lcd.print(x);
-  else Serial.print(x);
+  Serial.print(x);
 }
-void tprint(char x){
+void tprint(char x) {
   if (!sim) lcd.print(x);
-  else Serial.print(x);
+  Serial.print(x);
 }
-void tprint(int x){
+void tprint(int x) {
   if (!sim) lcd.print(x);
-  else Serial.print(x);
+  Serial.print(x);
 }
-void tprint(IPAddress x){
+void tprint(IPAddress x) {
   if (!sim) lcd.print(x);
-  else Serial.print(x);
+  Serial.print(x);
 }
-void tclear(){
+void tclear() {
   if (!sim) lcd.clear();
-  else Serial.println();
+  Serial.println();
 }
 String hide(String str) {
   String ret = "";
-  for (int i=0;i<str.length();i++)ret+="*";
+  for (int i = 0; i < str.length(); i++)ret += "*";
   return ret;
 }
 unsigned char h2int(char c) {
-    if (c >= '0' && c <='9'){
-        return((unsigned char)c - '0');
-    }
-    if (c >= 'a' && c <='f'){
-        return((unsigned char)c - 'a' + 10);
-    }
-    if (c >= 'A' && c <='F'){
-        return((unsigned char)c - 'A' + 10);
-    }
-    return(0);
+  if (c >= '0' && c <= '9') {
+    return ((unsigned char)c - '0');
+  }
+  if (c >= 'a' && c <= 'f') {
+    return ((unsigned char)c - 'a' + 10);
+  }
+  if (c >= 'A' && c <= 'F') {
+    return ((unsigned char)c - 'A' + 10);
+  }
+  return (0);
 }
 String urldecode(String str) {
-    String encodedString="";
-    char c;
-    char code0;
-    char code1;
-    for (int i =0; i < str.length(); i++){
-        c=str.charAt(i);
-      if (c == '+'){
-        encodedString+=' ';  
-      }else if (c == '%') {
-        i++;
-        code0=str.charAt(i);
-        i++;
-        code1=str.charAt(i);
-        c = (h2int(code0) << 4) | h2int(code1);
-        encodedString+=c;
-      } else{
-        encodedString+=c;  
-      }
-      yield();
+  String encodedString = "";
+  char c;
+  char code0;
+  char code1;
+  for (int i = 0; i < str.length(); i++) {
+    c = str.charAt(i);
+    if (c == '+') {
+      encodedString += ' ';
+    } else if (c == '%') {
+      i++;
+      code0 = str.charAt(i);
+      i++;
+      code1 = str.charAt(i);
+      c = (h2int(code0) << 4) | h2int(code1);
+      encodedString += c;
+    } else {
+      encodedString += c;
     }
-   return encodedString;
+    yield();
+  }
+  return encodedString;
 }
