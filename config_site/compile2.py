02c358e81ec9ab569d86b80fefe721f36c63809c
3LucasZ <lucas.zheng@warriorlife.net>
2024-05-23 15:09:52 -0700

    New OP compiler for html -> cpp

--- config_site/compile2.py
+++ config_site/compile2.py
@@ -4,32 +4,55 @@ outputFile = "ret.c"
 delimeter1 = "[[[[["
 delimeter2 = "]]]]]"
 
-
-
 open(outputFile, "w").close() #clear the output file
-for name in inputFiles:
-  f = open(name + ".html", "r")
-  fin = f.read()
-  fout = ["\"" + x + "\\" + "n" + "\"" for x in fin.replace("\"","\'").splitlines()]
-  
+for inputFile in inputFiles: #loop through input files
+  params = []
+  fileContent = open(inputFile, "r").read()
+  formattedFileLines = ["\"" + fileLine + "\\" + "n" + "\"" for fileLine in fileContent.replace("\"","\'").splitlines()]
 
-middle = []
-with open(outputFile, "a") as f:
-    f.write("const char* " + name + "Html = ")
-    for i in fout:
-      split = i.split(delimeter1)
-      if (len(split)==2):
-        inner_str = split[1].split(delimeter2)[0]
-        i = i.replace(delimeter1+inner_str+delimeter2,"\" + "+inner_str +" + \"")
-      f.write("\n"+i)
+  middle = []
+  for line in formattedFileLines:
+    split = line.split(delimeter1)
+    if (len(split)==2):
+      param = split[1].split(delimeter2)[0]
+      if (param not in params):
+        params.append(param)
+      line = line.replace(delimeter1+param+delimeter2,"\" + "+param +" + \"")
+    middle.append(line)
+  print(params)
+
+  start = "String getIndex("
+  for i in range(len(params)):
+    start += "String " + params[i]
+    if (i!=len(params)-1):
+      start += ", "
+  start += ") {"
+
+  with open(outputFile, "a") as f:
+    f.write(start + "\n")
+    f.write("String ret = ")
+    for line in middle: f.write(line + "\n")
     f.write(";")
-    f.write("\n")
-
-start = "string getIndex"
-
-open(outputFile, "w").close()
-for name in inputFiles:
-  f = open(name + ".html", "r")
-  fin = f.read()
-  fout = ["\"" + x + "\\" + "n" + "\"" for x in fin.replace("\"","\'").splitlines()]
+    f.write("return ret;")
+    f.write("}")
   
+
+
+
+
+
+
+# with open(outputFile, "a") as f:
+#     f.write("const char* " + inputFile + "Html = ")
+#     for i in fout:
+#       split = i.split(delimeter1)
+#       if (len(split)==2):
+#         param = split[1].split(delimeter2)[0]
+#         i = i.replace(delimeter1+param+delimeter2,"\" + "+param +" + \"")
+#       f.write("\n"+i)
+#     f.write(";")
+#     f.write("\n")
+
+
+
+
